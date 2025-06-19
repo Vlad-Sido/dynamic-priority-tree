7b003475ac85852494e6fe00f29c87dc6b9f0587
Vlad-Sido <vladissid2004@gmail.com>
2025-06-19 17:56:33 +0300

    Создание javadoc

--- demo/src/main/java/com/example/DynamicPriorityQueue.java
+++ demo/src/main/java/com/example/DynamicPriorityQueue.java
@@ -2,106 +2,36 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Имплементация динамической {@linkplain java.util.PriorityQueue очереди приоритета}. 
+ * Для приоритета используется {@linkplain PriorityUpdater функция, возвращающая целое число}.
+ * Корнем очереди считается элемент с наименьшим приоритетом.
+ * Элемент не может быть добавлен в очередь, если он уже находится в ней.
+ */
 public class DynamicPriorityQueue<T> {
-    protected class Node
-    {
-        protected T value;
-        protected int key;
-        protected ArrayList<Node> children;
-        protected PriorityUpdater updater;
-        protected Node parent;
-        public Node(T value, PriorityUpdater updater, ArrayList<Node> children)
-        {
-            this.value = value;
-            this.updater = updater;
-            this.children = children;
-            key = updater.priority();
-        }
-        public String toString()
-        {
-            return String.format("v: %s, k: %d", value, key);
-        }
-    }
-    protected HashMap<T, Node> elements = new HashMap<>();
-    protected Node root = null;
-
+    /**
+     * Создает пустую очередь
+     */
     public DynamicPriorityQueue()
     {
-
     }
+    /**
+     * Создает очередь с данными элементами
+     * Ключом словаря являются элементы, а значением: либо {@linkplain PriorityUpdater функция, возвращающая целое число}, либо целое число.
+     */
     public DynamicPriorityQueue(Map<T, ?> priorities)
     {
         addAll(priorities);
     }
 
-    // Вспомогательные методы
 
-    // Слияние деревьев
-    protected Node meld(Node elem1, Node elem2)
-    {
-        // Если одно пустое, возвращаем другое
-        if (elem1 == null)
-            return elem2;
-        if (elem2 == null)
-            return elem1;
-        // Иначе, добавляем более крупное как элемент меньшего
-        if (elem1.key < elem2.key)
-        {
-            elem1.children.add(elem2);
-            elem2.parent = elem1;
-            return elem1;
-        }
-        elem2.children.add(elem1);
-        elem1.parent = elem2;
-        return elem2;
-    }
-
-    // Слияние деревьев парами
-    protected Node mergePairs(ArrayList<Node> list)
-    {
-        if (list.isEmpty())
-            return null;
-        if (list.size() == 1)
-        {
-            list.get(0).parent = null;
-            return list.get(0);
-        }
-        var node1 = list.removeFirst();
-        var node2 = list.removeFirst();
-        node1.parent = null;
-        node2.parent = null;
-        return meld(meld(node1, node2), mergePairs(list));
-    }
-
-    // Уменьшает ключ вершины
-    protected void decreaseKey(Node node, int newVal)
-    {
-        if (node.key == newVal)
-            return;
-        node.key = newVal;
-        if (node.parent == null)
-            return;
-        node.parent.children.remove(node);
-        node.parent = null;
-        root = meld(root, node);
-    }
-
-    // Вставить вершину и ее вернуть 
-    protected Node insertNode(T value, PriorityUpdater updater)
-    {
-        var newNode = new Node(value, updater, new ArrayList<>());
-        elements.put(value, newNode);
-        root = meld(newNode, root);
-        return newNode;
-    }
-    
-    // Использование как очередь
-
-    // Вставление нового элемента как слияние с корнем
+    /**
+     * Добавляет элемент в очередь
+     * @return {@code true} если его еще нет, иначе {@code false}
+     */
     public boolean offer(T value, PriorityUpdater updater) // PriorityQueue
     {
         if (elements.containsKey(value))
@@ -111,20 +41,36 @@ public class DynamicPriorityQueue<T> {
         root = meld(newNode, root);
         return true;
     }
+    /**
+     * Добавляет элемент в очередь
+     * @return {@code true} если его еще нет, иначе {@code false}
+     */
     public boolean add(T value, PriorityUpdater updater) // AbstractQueue
     {
         return offer(value, updater);
     }
+    /**
+     * Добавляет элемент в очередь, со статическим приоритетом.
+     * @return {@code true} если его еще нет, иначе {@code false}
+     */
     public boolean offer(T value, int priority) // PriorityQueue
     {
         return offer(value, () -> priority);
     }
+    /**
+     * Добавляет элемент в очередь, со статическим приоритетом.
+     * @return {@code true} если его еще нет, иначе {@code false}
+     */
     public boolean add(T value, int priority) // PriorityQueue
     {
         return offer(value, () -> priority);
     }
 
-    // Вставление множества
+    /**
+     * Добавляет все элементы из словаря в очередь
+     * Ключом словаря являются элементы, а значением: либо {@linkplain PriorityUpdater функция, возвращающая целое число}, либо целое число.
+     * @return {@code true} если хотя бы один был добавлен, иначе {@code false}
+     */
     public boolean addAll(Map<T, ?> priorities)
     {
         if (priorities.isEmpty())
@@ -146,23 +92,33 @@ public class DynamicPriorityQueue<T> {
         return res;
     }
 
-    // Нахождение минимума
+    /**
+     * Возвращает корень очереди, не удаляя его.
+     * Если очередь пуста, возвращает {@code null}
+     */
     public T peek() // PriorityQueue
     {
-        // Возвращает минимум если пусто, как и PriorityQueue.
+        // Возвращает null если пусто, как и PriorityQueue.
         if (root == null)
             return null;
         return root.value;
     }
+    /**
+     * Возвращает корень очереди, не удаляя его.
+     * Если очередь пуста, возвращает {@code null}
+     */
     public T element() // AbstractQueue
     {
         return peek();
     }
 
-    // Вытаскивание минимума
+    /**
+     * Возвращает корень очереди и удаляет его.
+     * Если очередь пуста, возвращает {@code null}
+     */
     public T poll() // PriorityQueue
     {
-        // Возвращает минимум если пусто, как и PriorityQueue.
+        // Возвращает null если пусто, как и PriorityQueue.
         if (root == null)
             return null;
         var res = root.value;
@@ -170,12 +126,19 @@ public class DynamicPriorityQueue<T> {
         root = mergePairs(root.children);
         return res;
     }
+    /**
+     * Возвращает корень очереди и удаляет его.
+     * Если очередь пуста, возвращает {@code null}
+     */
     public T remove() // AbstractQueue
     {
         return poll();
     }
 
-    // Удаляет какой-то объект
+    /**
+     * Удаляет элемент из очереди.
+     * @return {@code true} если он был, иначе {@code false}
+     */
     public boolean remove(Object o)
     {
         var node = elements.get(o);
@@ -186,44 +149,44 @@ public class DynamicPriorityQueue<T> {
         return true;
     }
 
-    // Очистка очереди
+    /**
+     * Упостушает очередь полностью
+     */
     public void clear()
     {
         root = null;
         elements.clear();
     }
 
-    // Имеет ли элемент
+    /**
+     * Проверяет, ести ли в очереди объект.
+     * @return {@code true} если он был, иначе {@code false}
+     */
     public boolean contains(Object o)
     {
         return elements.keySet().contains(o);
     }
 
-    // Итератор
-    public Iterator<T> iterator() {
-        return elements.keySet().iterator();
-    }
-
-    // Размер
+    /**
+     * Возвращает количество элементов в очереди.
+     */
     public int size() {
         return elements.size();
     }
 
-    // Пустой ли
+    /**
+     * Проверяет, пуста ли очередь.
+     * @return {@code true} если в очереди нет элементов, иначе {@code false}
+     */
     public boolean isEmpty()
     {
         return root == null;
     }
 
-    // В массив
-    public Object[] toArray()
-    {
-        return elements.keySet().toArray();
-    }
-
-    // Уникальное для динамической очереди
-
-    // Обновить элемент. false если его нет, иначе true.
+    /**
+     * Обновляет приоритет определенного значения.
+     * @return {@code true} если в очереди он есть, иначе {@code false}
+     */
     public boolean update(Object o)
     {
         var node = elements.get(o);
@@ -243,7 +206,9 @@ public class DynamicPriorityQueue<T> {
         return true;
     }
 
-    // Обновить все значения
+    /**
+     * Обновляет приоритет всех значений.
+     */
     public void updateAll()
     {
         var elementMap = elements.entrySet().stream().collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue().updater));
@@ -251,8 +216,83 @@ public class DynamicPriorityQueue<T> {
         addAll(elementMap);
     }
 
-    public String toString()
+    private class Node
     {
-        return String.format("head: %s\nelements: %s", root, elements.values().toString());
+        private T value;
+        private int key;
+        private ArrayList<Node> children;
+        private PriorityUpdater updater;
+        private Node parent;
+        public Node(T value, PriorityUpdater updater, ArrayList<Node> children)
+        {
+            this.value = value;
+            this.updater = updater;
+            this.children = children;
+            key = updater.priority();
+        }
+        public String toString()
+        {
+            return String.format("v: %s, k: %d", value, key);
+        }
+    }
+    private HashMap<T, Node> elements = new HashMap<>();
+    private Node root = null;
+
+    // Вспомогательные методы
+
+    /**
+     *  Слияние деревьев
+     */
+    private Node meld(Node elem1, Node elem2)
+    {
+        // Если одно пустое, возвращаем другое
+        if (elem1 == null)
+            return elem2;
+        if (elem2 == null)
+            return elem1;
+        // Иначе, добавляем более крупное как элемент меньшего
+        if (elem1.key < elem2.key)
+        {
+            elem1.children.add(elem2);
+            elem2.parent = elem1;
+            return elem1;
+        }
+        elem2.children.add(elem1);
+        elem1.parent = elem2;
+        return elem2;
+    }
+
+    /**
+     *  Слияние деревьев парами
+     */
+    private Node mergePairs(ArrayList<Node> list)
+    {
+        if (list.isEmpty())
+            return null;
+        if (list.size() == 1)
+        {
+            list.get(0).parent = null;
+            return list.get(0);
+        }
+        var node1 = list.removeFirst();
+        var node2 = list.removeFirst();
+        node1.parent = null;
+        node2.parent = null;
+        return meld(meld(node1, node2), mergePairs(list));
+    }
+
+    /**
+     *  Уменьшает ключ вершины
+     */
+    private void decreaseKey(Node node, int newVal)
+    {
+        if (node.key == newVal)
+            return;
+        node.key = newVal;
+        if (node.parent == null)
+            return;
+        node.parent.children.remove(node);
+        node.parent = null;
+        root = meld(root, node);
     }
 }
--- demo/src/main/java/com/example/PriorityUpdater.java
+++ demo/src/main/java/com/example/PriorityUpdater.java
@@ -1,6 +1,15 @@
 package com.example;
 
+/**
+ * Представитель приоритета, функциональный интерфейс.
+ * Для правильной работы, функция должна возвращать одно и то же число, если вызвана несколько раз подряд.
+ * Единственная функция: {@link #priority()}.
+ * Практически эквивалентно {@linkplain java.util.function.Supplier Supplier}<{@linkplain Integer}>.
+ */
 @FunctionalInterface
 public interface PriorityUpdater {
+    /**
+     * Предоставление приоритета
+     */
     int priority();
 }
